de15b2f65b2066abe9292f6f3a5b9e430d7ca104
natali <vesnaluna2015@yandex.ru>
2022-11-30 13:32:03 +0300

    задачи

---
+++ seminar2/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+int [] array = {digit};
+array[0] = digit;
+Console.WriteLine(array[3]);
+
+
---
+++ seminar2/task9/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+int digit2 = number % 10;
+int digit1 = number / 10;
+Console.WriteLine($"Первая цифра = {digit1}, вторая цифрв = { digit2}");
+if (digit1 > digit2)
+{
+    Console.WriteLine(digit1);
+}
+else
+{
+    Console.WriteLine(digit2);
+}
