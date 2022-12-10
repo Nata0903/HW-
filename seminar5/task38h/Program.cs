7985cd2cbc89bc4ac9e98c7358ce1a231abdcfdc
natali <vesnaluna2015@yandex.ru>
2022-12-11 00:35:17 +0300

    Задачи 34/36/38

---
+++ seminar5/task38h/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 38: Задайте массив натуральных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] GetRandomArray(int size, int leftRange, int rightRange) 
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < array.Length; i++ )
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}   
+const int SIZE =5;
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 10;
+
+int[] arr = GetRandomArray(SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine(string.Join(",", arr));
+
+int res = 0;
+int arr[i] = max;
+int ar[i] = min;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max ! arr[i] < min)
+    {
+            res =arr[i] - ar[i];
+    }
+} res++;
+Console.WriteLine(res);
